8ee4a3820d0619488defaead64ac63b174f355a6
zargot <zargot.ripley@gmail.com>
2022-02-14 14:14:27 -0500

    fix variable size fetch

--- odbc.nim
+++ odbc.nim
@@ -167,6 +167,21 @@ proc fetchRow*(qry: SQLQuery, row: var SQLRow): bool =
       rptOnErr(qry.con.reporting, res, "SQLGetData", qry.handle, SQL_HANDLE_STMT.TSqlSmallInt)
       when defined(odbcdebug): echo &"SQLGetData returned {res}, with indicator: {indicator}"
 
+      # handle variable size data
+      if res == SQL_SUCCESS_WITH_INFO:
+        if indicator != SQL_NO_TOTAL and indicator != SQL_NULL_DATA:
+          let total = indicator
+          let retrieved = size
+          let rem = total - retrieved
+          let actualSize = total
+          qry.dataBuf = realloc(qry.dataBuf, actualSize)
+          let p = cast[pointer](cast[uint](qry.dataBuf) + size.uint)
+          res = SQLGetData(qry.handle, colIdx.SqlUSmallInt, colDetail.cType,
+                           p, rem, addr(indicator))
+          rptOnErr(qry.con.reporting, res, "SQLGetData", qry.handle, SQL_HANDLE_STMT.TSqlSmallInt)
+          when defined(odbcdebug): echo &"SQLGetData returned {res}, with indicator: {indicator}"
+          indicator = actualSize
+
       if indicator == SQL_NO_TOTAL:
         when defined(odbcdebug): echo "No Total"
         else: discard
@@ -279,10 +294,6 @@ proc getColDetails(qry: SQLQuery, colID: int): SQLField =
     decimalDigits: TSqlSmallInt
     nullable: TSqlSmallInt
     retval = SQLDescribeCol(qry.handle, colID.TSqlSmallInt, colName.cstring, buflen, nameLen, sqlDataType, columnSize, decimalDigits, nullable)
-  
-  if columnSize == 0:
-    # The column size cannot be determined by the driver.
-    columnSize = sqlDataType.TSqlULen
 
   rptOnErr(qry.con.reporting, retval, "SQLDescribeCol", qry.handle, SQL_HANDLE_STMT.TSqlSmallInt)
   colName.setLen(nameLen)
