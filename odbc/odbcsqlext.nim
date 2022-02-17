ae43ec737930eb2823bb66c61bd326a9fd36bbab
zargot <zargot.ripley@gmail.com>
2022-02-17 08:11:14 -0500

    add SQLDriverConnectW

--- odbc/odbcsqlext.nim
+++ odbc/odbcsqlext.nim
@@ -7,6 +7,10 @@ include std/odbcsql
 
 {.push importc, dynlib: odbclib.}
 
+proc SQLDriverConnectW*(hdbc: SqlHDBC, hwnd: SqlHWND, szCsin: WideCString,
+    szCLen: TSqlSmallInt, szCsout: WideCString, cbCSMax: TSqlSmallInt,
+    cbCsOut: var TSqlSmallInt, f: SqlUSmallInt): TSqlSmallInt
+
 proc SQLMoreResults*(StatementHandle: SqlHStmt): TSqlSmallInt
 
 proc SQLGetTypeInfo*(StatementHandle: SqlHStmt,
